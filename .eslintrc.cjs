ea5933ba3169dda20b78777f51603d0e5d94e35c
IlyaKhokhanov <hohanov.ilya@gmail.com>
2023-10-27 16:07:08 +0300

    chore: setup the project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,40 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 2,
+    'react/jsx-props-no-spreading': 'off',
   },
-}
+  overrides: [
+    {
+      rules: {
+        'no-param-reassign': 'off',
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: './eslintrc.js',
+};
