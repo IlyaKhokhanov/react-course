40a8a089f1858f539358157e66aa2c636a10b84f
IlyaKhokhanov <hohanov.ilya@gmail.com>
2023-11-01 00:54:56 +0300

    fix: fix husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,40 +1,47 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  extends: [
-    'standard',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'airbnb',
-    'airbnb-typescript',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   rules: {
-    'prettier/prettier': 'error',
-    '@typescript-eslint/no-explicit-any': 2,
-    'react/jsx-props-no-spreading': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'comma-dangle': ['error', 'only-multiline'],
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-var-requires': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
-  overrides: [
-    {
-      rules: {
-        'no-param-reassign': 'off',
-      },
-    },
-  ],
   settings: {
     react: {
+      pragma: 'React',
       version: 'detect',
     },
   },
-  ignorePatterns: './eslintrc.js',
 };
